c3404885e4753285e222105f3bbec38bd2b77d1f
Nanda Wijaya Putra <nandawijaya@apps.ipb.ac.id>
2023-10-25 10:29:40 +0700

    update auth

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,5 +1,94 @@
-export default function() {
+import Image from "next/image";
+import React from "react";
+import logo from '../../../styles/img/logo.png'
+import hideIcon from '../../../images/hide.png'
+import showIcon from '../../../images/show.png'
+
+import {
+    SimpleGrid, 
+    Container,
+    Box,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    Input,
+    Button,
+    InputGroup,
+    InputRightElement,
+    Checkbox,
+    ring,
+  } from '@chakra-ui/react'
+
+export default function Login() {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
     return (
-        <h1>Login Page</h1>
+        <>
+        <div className="login">
+            <Container maxW={'40%'}>
+                <Image src={logo} alt="logo"/>
+            </Container>
+            <Container maxW={'50%'} style={
+                {
+                    backgroundColor: "white",
+                    borderRadius: "100px",
+                    padding: "100px"
+                }
+            }>
+                <h1 style={
+                    {
+                        textAlign: "center",
+                        fontSize: "40px",
+                        color: "#4E7031",
+                        // fontFamily: "Poppins",
+                        fontWeight: "700",
+                        fontStyle: "normal",
+                        lineHeight: "49px",
+                        letterSpacing: "-0.9px",
+                    }
+                }>Ayo Bergabung!</h1>
+                <p style={
+                    {
+                        textAlign: "center",
+                        color: "#4E7031",
+                        fontFamily: "DM Sans",
+                        fontSize: "20px",
+                        fontStyle: "normal",
+                        fontWeight: "500",
+                        lineHeight: "56px",
+                        letterSpacing: "-0.4px",
+                        marginBottom: "50px"
+                    }
+                }>Belum punya akun? <Button color='#4E7031' variant='link'>Daftar</Button></p>   
+                <FormControl color="#4E7031">
+                    <FormLabel>Username</FormLabel>
+                    <Input type='text' placeholder="Username"/>
+                </FormControl>
+                <FormControl marginTop={5} color="#4E7031">
+                    <FormLabel >Password</FormLabel>
+                    <InputGroup size="md">
+                        <Input 
+                            type={show ? 'text' : 'password'} 
+                            placeholder="Password" 
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button size="sm" onClick={handleClick} variant={'link'}>
+                                {show ? <Image src={hideIcon} alt="hide" width={20}/> : <Image src={showIcon} alt="show" width={20}/> }
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                </FormControl>
+                <SimpleGrid columns={2} spacing={10} marginTop={5}>
+                    <FormControl color="#4E7031">
+                        <Checkbox colorScheme="green" defaultChecked>ingatkan saya</Checkbox>
+                    </FormControl>
+                    <FormControl color="#4E7031" textAlign={'right'}>
+                        <Button color='#4E7031' variant='link'>Lupa Password?</Button>
+                    </FormControl>
+                </SimpleGrid>
+                <Button type="submit" color="white" background="#4E7031" size="lg" width={"100%"} height={50} marginTop={10}>Sign In</Button>
+            </Container>
+        </div>
+        </>
     )
 }
--- src/pages/auth/register/index.jsx
+++ src/pages/auth/register/index.jsx
@@ -1,5 +1,108 @@
-export default function() {
+import Image from "next/image";
+import React from "react";
+import logo from '../../../styles/img/logo.png'
+import hideIcon from '../../../images/hide.png'
+import showIcon from '../../../images/show.png'
+
+import {
+    SimpleGrid, 
+    Container,
+    Box,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    Input,
+    Button,
+    InputGroup,
+    InputRightElement,
+    Checkbox,
+  } from '@chakra-ui/react'
+
+export default function Login() {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
     return (
-        <h1>Register Page</h1>
+        <>
+        <div className="register">
+            <Container maxW={'90%'} style={
+                {
+                    backgroundColor: "white",
+                    borderRadius: "100px",
+                    padding: "20px 50px 50px 20px"
+
+                }
+            }>
+                <Image src={logo} alt="logo" width={274}/>
+                <h1 style={
+                    {
+                        textAlign: "center",
+                        fontSize: "40px",
+                        color: "#4E7031",
+                        // fontFamily: "Poppins",
+                        fontWeight: "700",
+                        fontStyle: "normal",
+                        lineHeight: "49px",
+                        letterSpacing: "-0.9px",
+                    }
+                }>Ayo Bergabung!</h1>
+                <p style={
+                    {
+                        textAlign: "center",
+                        color: "#4E7031",
+                        fontFamily: "DM Sans",
+                        fontSize: "20px",
+                        fontStyle: "normal",
+                        fontWeight: "500",
+                        lineHeight: "56px",
+                        letterSpacing: "-0.4px",
+                        marginBottom: "10px"
+                    }
+                }>Sudah punya akun? <Button color='#4E7031' variant='link'>Masuk</Button></p>
+                <Container maxW={'80%'}>
+                    <SimpleGrid columns={2} spacing={10}>
+                        <FormControl color="#4E7031">
+                            <FormLabel maxH={4}>Nama Awal</FormLabel>
+                            <Input type='text' placeholder="nama awal" size='xs'/>
+                        </FormControl>
+                        <FormControl color="#4E7031">
+                            <FormLabel maxH={4}>Nama Akhir</FormLabel>
+                            <Input type='text' placeholder="nama awal" size='xs'/>
+                        </FormControl>
+                    </SimpleGrid>
+                    <FormControl color="#4E7031" marginTop={4}>
+                        <FormLabel maxH={4}>Alamat Email</FormLabel>
+                        <Input type='text' placeholder="email@mail.com" size='xs' />
+                    </FormControl>
+                    <FormControl color="#4E7031" marginTop={4}>
+                        <FormLabel maxH={4}>Nomor Induk Penduduk (NIK)</FormLabel>
+                        <Input type='text' placeholder="13 karakter" size='xs'/>
+                    </FormControl>
+                    <FormControl color="#4E7031" marginTop={4}>
+                        <FormLabel maxH={4}>Nomor Telephone</FormLabel>
+                        <Input type='text' placeholder="+62xxxx" size='xs'/>
+                    </FormControl>
+                    <FormControl color="#4E7031" marginTop={4}>
+                        <FormLabel maxH={4}>Username</FormLabel>
+                        <Input type='text' placeholder="username" size='xs'/>
+                    </FormControl>
+                    <FormControl color="#4E7031" marginTop={4}>
+                        <FormLabel maxH={4}>Password</FormLabel>
+                        <InputGroup size="xs">
+                            <Input 
+                                type={show ? 'text' : 'password'} 
+                                placeholder="Password" 
+                            />
+                            <InputRightElement width="4.5rem">
+                                <Button size="xs" onClick={handleClick} variant={'link'}>
+                                    {show ? <Image src={hideIcon} alt="hide" width={20}/> : <Image src={showIcon} alt="show" width={20}/> }
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                    </FormControl>
+                    <Button type="submit" color="white" background="#4E7031" size="lg" width={"100%"} height={50} marginTop={7}>Submit</Button>
+                </Container>   
+            </Container>
+        </div>
+        </>
     )
 }
