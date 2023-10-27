c007d40857176f9642415a3814effb43acdfead2
DanyFadhilah <fadilahdany@gmail.com>
2023-10-27 17:47:18 +0700

    Homepage ver Baru

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
